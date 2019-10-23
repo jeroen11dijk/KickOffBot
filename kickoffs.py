84efff6926974a3f20c5c958d601f802cbb8f0e0
Jeroen van Dijk <jeroen11dijk@gmail.com>
2019-10-23 19:37:38 +0200

    Added a working nombot

--- kickoffs.py
+++ kickoffs.py
@@ -2,8 +2,7 @@ from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
 from rlbot.utils.structures.game_data_struct import GameTickPacket
 
 from derevo.derevo import Derevo
-
-bots = ["derevo"]
+from NomBot.NomBot import NomBot_1_5
 
 
 class MyBot(BaseAgent):
@@ -13,14 +12,22 @@ class MyBot(BaseAgent):
         self.name = name
         self.team = team
         self.bot = Derevo(name, team, index)
+        self.bot2 = NomBot_1_5(name, team, index)
 
     def initialize_agent(self):
-        self.bot.get_ball_prediction_struct = self.bot.get_ball_prediction_struct
-        self.bot.get_field_info = self.get_field_info
-        self.bot.renderer = self.renderer
-        self.bot.send_quick_chat = self.send_quick_chat
-        self.bot.initialize_agent()
+        self.register_bot(self.bot)
+        self.register_bot(self.bot2)
+        print(self.bot2.get_ball_prediction_struct())
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         self.bot.get_rigid_body_tick = self.get_rigid_body_tick
-        return self.bot.get_output(packet)
+        self.bot2.get_rigid_body_tick = self.get_rigid_body_tick
+        # return self.bot.get_output(packet)
+        return self.bot2.get_output(packet)
+
+    def register_bot(self, bot):
+        bot.get_ball_prediction_struct = self.get_ball_prediction_struct
+        bot.get_field_info = self.get_field_info
+        bot.renderer = self.renderer
+        bot.send_quick_chat = self.send_quick_chat
+        bot.initialize_agent()
