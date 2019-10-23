39d3d4c6a24f1d78367b28341180c0e3864e6d52
Jeroen van Dijk <jeroen11dijk@gmail.com>
2019-10-23 20:44:03 +0200

    Setup the rest but statesetting does not quite work yet. Posted in #issues-and-bugs

--- kickoffs.py
+++ kickoffs.py
@@ -1,8 +1,9 @@
 from rlbot.agents.base_agent import BaseAgent, SimpleControllerState
-from rlbot.utils.structures.game_data_struct import GameTickPacket
+from rlbot.utils.game_state_util import GameState, BallState, Physics, Vector3
+from rlbot.utils.structures.game_data_struct import GameTickPacket, Vector3, Physics
 
-from derevo.derevo import Derevo
 from NomBot.NomBot import NomBot_1_5
+from derevo.derevo import Derevo
 
 
 class MyBot(BaseAgent):
@@ -11,18 +12,27 @@ class MyBot(BaseAgent):
         super().__init__(name, team, index)
         self.name = name
         self.team = team
+        self.prev_kickoff = True
+        self.timeout = 0
         self.bot = Derevo(name, team, index)
         self.bot2 = NomBot_1_5(name, team, index)
 
     def initialize_agent(self):
         self.register_bot(self.bot)
         self.register_bot(self.bot2)
-        print(self.bot2.get_ball_prediction_struct())
 
     def get_output(self, packet: GameTickPacket) -> SimpleControllerState:
         self.bot.get_rigid_body_tick = self.get_rigid_body_tick
         self.bot2.get_rigid_body_tick = self.get_rigid_body_tick
-        # return self.bot.get_output(packet)
+        if self.prev_kickoff and not packet.game_info.is_kickoff_pause:
+            self.timeout = packet.game_info.seconds_elapsed + 2
+        self.prev_kickoff = packet.game_info.is_kickoff_pause
+        if packet.game_info.seconds_elapsed > self.timeout:
+            if packet.game_ball.physics.location.y > 0:
+                ball_state = BallState(Physics(location=Vector3(0, 5150, 100)))
+            else:
+                ball_state = BallState(Physics(location=Vector3(0, -5150, 100)))
+            self.set_game_state(GameState(ball=ball_state))
         return self.bot2.get_output(packet)
 
     def register_bot(self, bot):
